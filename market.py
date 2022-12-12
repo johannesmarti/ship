d4d187abfee75909b1158d20515556b48cd72af1
Johannes Marti <johannes.marti@gmail.com>
2022-12-12 21:07:15 +0000

    improving usage of ElasticBundle

--- market.py
+++ market.py
@@ -18,14 +18,33 @@ Market = Callable[[Iterable[Participant],Prices],Prices]
 
 
 Elasticity = List[float]
-ElasticBundle = Tuple[Bundle,Elasticity]
+
+class ElasticBundle:
+    value : Bundle
+    elasticity : Elasticity
+    def __init__(value, elasticity):
+        self.value = value
+        self.elasticity = elasticity
+
+    @classmethod
+    def zero(cls,zero_base):
+        return ElasticBundle(zero_base, zero_base)
+
+    def __add__(self, other):
+        if isinstance(other, ElasticBundle):
+            return ElasticBundle(self.value + other.value,
+                                 self.elasticity + other.elasticity)
+        else:
+            raise ValueError("ElasticBundles can only be added to other ElasticBundles");
+
 
 class ElasticityEstimatingParticipant(Participant, Protocol):
     def participate_and_estimate(self, prices : Prices) -> ElasticBundle:
         ...
 
     def participate(self, prices : Prices) -> Bundle:
-        return self.participate_and_estimate(prices)[0]
+        return self.participate_and_estimate(prices).value
+
 
 EEP = ElasticityEstimatingParticipant
 
@@ -39,37 +58,34 @@ iterations : int = 0
 
 def one_iteration(participants : Iterable[EEP], prices : Prices) -> ElasticBundle:
     n = len(prices)
-    b = []
-    e = []
+    eb = ElasticBundle.zero([0])
     for p in participants:
-        (db,de) = p.participate_and_estimate(prices)
-        b += db
-        e += de
+        eb += p.participate_and_estimate(prices)
     iterations += 1
-    return (b,e)
+    return eb
 
-def update_prices(prices : Prices, error : Bundle, elasticity : Elasticity, t : float = 1) -> Prices:
+def update_prices(prices : Prices, error : ElasticBundle, t : float = 1) -> Prices:
     return prices #- 0.1 * error
 
 def simple_market(participants : Iterable[EEP], prices : Prices, epsilon : float = 0.1) -> Prices:
-    (error,elasticity) = one_iteration(participants, prices)
-    while norm(error) >= epsilon:
-        prices = update_prices(prices, error, elasticity)
-        (error, elasticity) = one_iteration(participants, prices)
+    error = one_iteration(participants, prices)
+    while norm(error.value) >= epsilon:
+        prices = update_prices(prices, error)
+        error = one_iteration(participants, prices)
     return prices
 
-def line_search(participants : Iterable[EEP], prices : Prices, error : Bundle, elasticity : Elasticity, t : float = 1, alpha : float = 1, beta : float = 0.5) -> Tuple[Prices,Bundle,Elasticity]:
-    next_prices = update_prices(prices, error, elasticity, t)
-    (next_error, next_elasticity) = one_iteration(participants, next_prices)
-    while next_error >= error: # * alpha:
+def line_search(participants : Iterable[EEP], prices : Prices, error : ElasticBundle, t : float = 1, alpha : float = 1, beta : float = 0.5) -> Tuple[Prices,ElasticBundle]:
+    next_prices = update_prices(prices, error, t)
+    next_error = one_iteration(participants, next_prices)
+    while next_error.value >= error.value: # * alpha:
         t *= beta
-        next_prices = update_prices(prices, error, elasticity, t)
-        (next_error,next_elasticity) = one_iteration(participants, next_prices)
-    return (next_prices, next_error, next_elasticity)
+        next_prices = update_prices(prices, error, t)
+        next_error = one_iteration(participants, next_prices)
+    return (next_prices, next_error)
 
 def line_search_market(participants : Iterable[EEP], prices : Prices, epsilon : float = 0.1) -> Prices:
-    (error,elasticity) = one_iteration(participants, prices)
-    while norm(error) >= epsilon:
-        (prices, error, elasticity) = line_search(participants, prices, error, elasticity)
+    error = one_iteration(participants, prices)
+    while norm(error.value) >= epsilon:
+        (prices, error) = line_search(participants, prices, error)
     return prices
 
