8f742379e9067e01b7716026f113c687e4386944
Johannes Marti <johannes.marti@gmail.com>
2022-12-13 12:59:42 +0000

    added some numpy stuff

--- market.py
+++ market.py
@@ -1,14 +1,15 @@
-from typing import Callable,Iterable,List,Protocol,Tuple
+import numpy as np
+from numpy.linalg import norm
 
-#class Market(Protocol):
-#    def register_participant(self, participant : MarketParticipant) -> None:
-#        ...
-#
-#    def compute_equilibrium(self, prices : List[float]) -> prices
+from typing import Callable,Iterable,Protocol,Tuple
+import numpy.typing as npt
 
 
-Prices = List[float]
-Bundle = List[float]
+ScalarType = np.float64
+#ScalarType = float
+
+Prices = npt.NDArray[ScalarType]
+Bundle = npt.NDArray[ScalarType]
 
 class Participant(Protocol):
     def participate(self, prices : Prices) -> Bundle:
@@ -17,25 +18,37 @@ class Participant(Protocol):
 Market = Callable[[Iterable[Participant],Prices],Prices]
 
 
-Elasticity = List[float]
+Elasticity = npt.NDArray[ScalarType]
 
 class ElasticBundle:
     value : Bundle
     elasticity : Elasticity
     def __init__(value, elasticity):
+        assert np.all(elasticity > 0)
+        assert np.shape(value) == np.shape(elasticity)
         self.value = value
         self.elasticity = elasticity
 
     @classmethod
-    def zero(cls,zero_base):
-        return ElasticBundle(zero_base, zero_base)
+    def zero(cls,shape):
+        return ElasticBundle(np.zeros(shape), np.zeros(shape))
 
     def __add__(self, other):
-        if isinstance(other, ElasticBundle):
-            return ElasticBundle(self.value + other.value,
-                                 self.elasticity + other.elasticity)
-        else:
-            raise ValueError("ElasticBundles can only be added to other ElasticBundles");
+        assert other.shape() == self.shape()
+        assert isinstance(other, ElasticBundle)
+        print("add creates new object")
+        return ElasticBundle(self.value + other.value,
+                             self.elasticity + other.elasticity)
+
+    def __iadd__(self, other):
+        assert other.shape() == self.shape()
+        assert isinstance(other, ElasticBundle)
+        self.value += other.value
+        self.elasticity += other.elasticity
+        return self
+
+    def shape(self):
+        return self.value.shape
 
 
 class ElasticityEstimatingParticipant(Participant, Protocol):
@@ -51,21 +64,17 @@ EEP = ElasticityEstimatingParticipant
 AdvancedMarket = Callable[[Iterable[ElasticityEstimatingParticipant],Prices],Prices]
 
 
-def norm(b : Bundle) -> float:
-    return 0
-
 iterations : int = 0
 
 def one_iteration(participants : Iterable[EEP], prices : Prices) -> ElasticBundle:
-    n = len(prices)
-    eb = ElasticBundle.zero([0])
+    eb = ElasticBundle.zero(prices.shape)
     for p in participants:
         eb += p.participate_and_estimate(prices)
     iterations += 1
     return eb
 
 def update_prices(prices : Prices, error : ElasticBundle, t : float = 1) -> Prices:
-    return prices #- 0.1 * error
+    return prices - (error.value/error.elasticity)
 
 def simple_market(participants : Iterable[EEP], prices : Prices, epsilon : float = 0.1) -> Prices:
     error = one_iteration(participants, prices)
@@ -77,7 +86,7 @@ def simple_market(participants : Iterable[EEP], prices : Prices, epsilon : float
 def line_search(participants : Iterable[EEP], prices : Prices, error : ElasticBundle, t : float = 1, alpha : float = 1, beta : float = 0.5) -> Tuple[Prices,ElasticBundle]:
     next_prices = update_prices(prices, error, t)
     next_error = one_iteration(participants, next_prices)
-    while next_error.value >= error.value: # * alpha:
+    while next_error.value >= alpha * error.value:
         t *= beta
         next_prices = update_prices(prices, error, t)
         next_error = one_iteration(participants, next_prices)
