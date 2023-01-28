f1f55e452b935675c3d112dfd0daba51e61051d3
Johannes Marti <johannes.marti@gmail.com>
2023-01-28 20:11:18 +0000

    some restructuring

--- runner.py
+++ runner.py
@@ -7,26 +7,26 @@ from market.base import *
 from market.line_search import *
 from market.estimating import *
 
-np.set_printoptions(precision=3,suppress=True,threshold=8)
+np.set_printoptions(precision=4,suppress=True,threshold=8)
 logging.basicConfig(level=logging.DEBUG, format='%(message)s (%(levelname)s)')
 #logging.basicConfig(level=logging.INFO, format='%(message)s (%(levelname)s)')
 #logging.basicConfig(level=logging.WARNING, format='%(message)s (%(levelname)s)')
 
-farm = Factory(np.array([6,1,0,0]), 10)
+farm = Factory(np.array([5,1,0,0]), 10)
 mine = Factory(np.array([0,-0.5,1.5,0]), 10)
 smith = Factory(np.array([0,-1,-2,2]), 20)
 woodCutter = Factory(np.array([0,5,0,-0.5]), 10)
 
-consumers = Consumer(np.array([2,1,0,3]), 1000)
+consumers = Consumer(np.array([2,1,0,2]), 1000)
 
 village = [farm,mine,smith,woodCutter,consumers]
 lazy_village = list(map(ElasticityFromVolumeParticipant, village))
 
-p0 = np.array([10000,10,10,1])
-epsilon = 0.001
+p0 = np.array([10,10,30,50])
+epsilon = 0.01
 
-#p = line_search_market(lazy_village, p0, epsilon, t=0.7)
-p = line_search_market(village, p0, epsilon, t=1.5)
+p = line_search_market(lazy_village, p0, epsilon, t=0.7)
+#p = line_search_market(village, p0, epsilon, t=1.3)
 #p = estimating_market(village, p0, epsilon, t=0.9, mixing=0.1)
 print("iterations:", get_iteration())
 print(p)
---
+++ snipplets/factory.py
@@ -0,0 +1,28 @@
+import numpy as np
+import logging
+
+import tools.derivative as sjd
+
+from participants.factory import *
+
+np.set_printoptions(precision=4,suppress=True,threshold=8)
+#logging.basicConfig(level=logging.DEBUG, format='%(message)s (%(levelname)s)')
+logging.basicConfig(level=logging.INFO, format='%(message)s (%(levelname)s)')
+
+p = np.array([10.0,10.0])
+
+farm = Factory(np.array([2,1]), 30)
+print(farm.participate_and_estimate(p))
+
+def f(p):
+    return farm.participate_and_estimate(p).value[0]
+
+sjd.print_estimation(f, p, 1)
+sjd.print_estimation(f, p, 0.1)
+sjd.print_estimation(f, p, 0.01)
+sjd.print_estimation(f, p, 0.001)
+#sjd.print_estimation(f, p, 0.0001)
+#sjd.print_estimation(f, p, 0.00001)
+#sjd.print_estimation(f, p, 0.000001)
+#sjd.print_estimation(f, p, 0.0000001)
+
