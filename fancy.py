fixing a bug in the computation of factorie

fancy.py
@@ -15,9 +15,9 @@ from core.schema import *
 from itertools import chain
 
 np.set_printoptions(precision=8,suppress=True,threshold=12)
-logging.basicConfig(level=logging.DEBUG, format='%(message)s (%(levelname)s)')
+#logging.basicConfig(level=logging.DEBUG, format='%(message)s (%(levelname)s)')
 #logging.basicConfig(level=logging.INFO, format='%(message)s (%(levelname)s)')
-#logging.basicConfig(level=logging.WARNING, format='%(message)s (%(levelname)s)')
+logging.basicConfig(level=logging.WARNING, format='%(message)s (%(levelname)s)')
 #logging.basicConfig(level=logging.ERROR, format='%(message)s (%(levelname)s)')
 
 local_schema = TradeGoodsSchema.from_lists(["food", "alcohol", "wood", "ore", "tools"],[])
@@ -35,9 +35,10 @@ spain = province_schema.province_of_name("Spain")
 def concat_map(func, it):
     return chain.from_iterable(map(func, it))
 
-trade_factors = 4*np.array([3, 5, 2, 2, 4])
+#trade_factors = 4*np.array([3, 5, 2, 2, 4])
 #trade_factors = 2*np.array([3, 5, 2, 2, 4])
-#trade_factors = np.array([3, 5, 2, 2, 4])
+#trade_factors = np.array([2.5, 4, 1.5, 2, 3.5])
+trade_factors = np.array([3, 3, 1.5, 2, 3])
 
 def set_up_merchants(home: int, foreign: int) -> Iterable[economy.TradeConfig]:
     def for_good(good: int) -> Iterable[economy.TradeConfig]:
@@ -52,8 +53,8 @@ province_configs = [
         800,
         np.array([2, 0.5, 1.2, 0, 0.7]),
         [
-            economy.FactoryConfig(np.array([4, 0.2, 1.8, 0, 0])), # Cow farm
-            economy.FactoryConfig(np.array([0, 0, -2, -1, 2])), # artisans
+            economy.FactoryConfig(np.array([5, 0.5, 1.5, 0, 0])), # Cow farm
+            economy.FactoryConfig(np.array([0, 0, -2, -1, 3])), # artisans
         ],
         list(set_up_merchants(switzerland, italy)) +
            list(set_up_merchants(switzerland, france)) +
@@ -66,10 +67,9 @@ province_configs = [
         6000,
         np.array([2.05, 0.5, 0.8, 0, 0.5]),
         [
-            economy.FactoryConfig(np.array([9, 0, 0, 0, 0])), # Po farm
+            economy.FactoryConfig(np.array([8, 0, 0, 0, 0])), # Po farm
             economy.FactoryConfig(np.array([0, 1.2, 0, 0, -0.1])), # wine
-            economy.FactoryConfig(np.array([-1, 0, 5, 0, 0])), # wood cutter
-            economy.FactoryConfig(np.array([0, 0, -0.5, 1.5, 0])), # Italian mine
+            economy.FactoryConfig(np.array([-1, 0, 4, 0, 0])), # wood cutter
             economy.FactoryConfig(np.array([0, 0, -1.9, -1.2, 1.8])), # smith
         ],
         list(set_up_merchants(italy, switzerland))
@@ -81,9 +81,9 @@ province_configs = [
         6500,
         np.array([2.02, 0.6, 1, 0.1, 0.5]),
         [
-            economy.FactoryConfig(np.array([8, 0, 0.8, 0, 0])), # Franch farming
+            economy.FactoryConfig(np.array([8, 0, 0, 0, 0])), # Franch farming
             economy.FactoryConfig(np.array([0, 1.2, 0, 0, -0.1])), # wine
-            economy.FactoryConfig(np.array([0, 0, 6, 0, -0.3])), # wood cutter
+            economy.FactoryConfig(np.array([0, 0, 5, 0, -0.3])), # wood cutter
             economy.FactoryConfig(np.array([0, 0, -0.5, 1.7, 0])), # French mine
             economy.FactoryConfig(np.array([0, 0, -1.9, -1.3, 1.9])), # smith
         ],
@@ -99,10 +99,10 @@ province_configs = [
         8000,
         np.array([2.01, 0.7, 1, 0.0, 0.6]),
         [
-            economy.FactoryConfig(np.array([7, 0, 0, 0, 0])), # farming
+            economy.FactoryConfig(np.array([6, 0, 1, 0, 0])), # farming
             economy.FactoryConfig(np.array([0, 1.1, 0, 0, -0.1])), # wine
             economy.FactoryConfig(np.array([0, 1.2, -0.5, 0, 0])), # beer
-            economy.FactoryConfig(np.array([0, 0, 7, 0, -0.2])), # wood cutter
+            economy.FactoryConfig(np.array([0, 0, 6, 0, -0.2])), # wood cutter
             economy.FactoryConfig(np.array([0, 0, -0.5, 1.6, 0])), # mine
             economy.FactoryConfig(np.array([0, 0, -2, -1.3, 2])), # smith
         ],
@@ -116,8 +116,8 @@ province_configs = [
         700,
         np.array([2.01, 0.6, 1.1, 0.0, 0.5]),
         [
-            economy.FactoryConfig(np.array([4.5, 0, 2, 0, 0])), # farming
-            economy.FactoryConfig(np.array([-1, 1, 0, 0, 0])), # liquor
+            economy.FactoryConfig(np.array([5, 0, 2, 0, 0])), # farming
+            economy.FactoryConfig(np.array([-1, 1.3, 0, 0, 0])), # liquor
             economy.FactoryConfig(np.array([0, 0, -1.9, -1.3, 1.9])), # smith
         ],
         list(set_up_merchants(austria, switzerland))
@@ -141,10 +141,10 @@ province_configs = [
         6600,
         np.array([1.97, 0.8, 1.1, 0, 0.49]),
         [
-            economy.FactoryConfig(np.array([6, 0, 0.4, 0, 0])), # farming
+            economy.FactoryConfig(np.array([5.5, 0, 0.4, 0, 0])), # farming
             economy.FactoryConfig(np.array([0, 1.2, -0.4, 0, 0])), # beer
             economy.FactoryConfig(np.array([0, 0, 6.5, 0, -0.1])), # wood cutter
-            economy.FactoryConfig(np.array([0, -0.1, -0.5, 1.9, 0])), # mine 2
+            economy.FactoryConfig(np.array([0, -0.1, -0.5, 1.9, 0])), # mine
             economy.FactoryConfig(np.array([0, 0, -2.0, -1.3, 1.9])), # smith
         ],
         list(set_up_merchants(england, italy))
@@ -157,9 +157,8 @@ province_configs = [
         4600,
         np.array([2.02, 0.5, 0.7, 0, 0.5]),
         [
-            economy.FactoryConfig(np.array([6.8, 0, 0.5, 0, 0])), # farming
-            economy.FactoryConfig(np.array([0, 1, 0, 0, -0.1])), # wine
-            economy.FactoryConfig(np.array([0, 0, 3, 0, -0.1])), # wood cutter
+            economy.FactoryConfig(np.array([6, 0, 0.5, 0, 0])), # farming
+            economy.FactoryConfig(np.array([0, 1.1, 0, 0, -0.1])), # wine
             economy.FactoryConfig(np.array([0, 0, -1.5, -1.3, 1.7])), # smith
         ],
         list(set_up_merchants(spain, italy))
@@ -172,7 +171,6 @@ econfig = economy.EconomyConfig(local_schema, province_schema, province_configs)
 econ = le.LaborEconomy.from_config(econfig)
 
 market_schema = econ.price_schema()
-#p0 = np.array([70.0,100,60,200.0,15,70,110,60,200,11])
 p0 = np.full(market_schema.global_width(), 100.0)
 epsilon = 0.1
 participants = list(econ.participants())
@@ -181,11 +179,12 @@ pt.set_global_table_logging_configuration(pt.PrettyTableConfiguration(
     schema = market_schema,
     #list_of_indices = list(range(market_schema.global_width()))
     list_of_indices = list(market_schema.ix_list_provinces_over_goods(
-        ["food", "alcohol", "labor"],
-        ["Switzerland", "The Netherlands"]))
+        None, None))
+    #    ["food", "alcohol", "ore"],
+    #    ["Switzerland", "Spain"]))
 ))
 
-scaling = ScalingConfiguration(set_to_price=100, norm_listing=market_schema.listing_of_good_in_province("food", "Germany"))
+scaling = ScalingConfiguration(set_to_price=10, norm_listing=market_schema.listing_of_good_in_province("food", "Germany"))
 
 def run_ls():
     config = ls.LineSearchConfiguration(
@@ -233,9 +232,9 @@ def run_eva():
 def run_adam():
     config = adam.AdamConfiguration(
                 epsilon=epsilon,
-                rate=0.0001,
+                rate=0.00001,
                 first_momentum_mixin = 0.02,
-                second_momentum_mixin = 0.001
+                second_momentum_mixin = 0.0001
              )
     p = adam.make_market(participants, p0, config)
     p = apply_price_scaling(p, scaling)
@@ -244,5 +243,5 @@ def run_adam():
     reset_iteration()
 
 
-run_eva() # 1714
+run_eva()
 

labor_economy/producer.py
@@ -7,7 +7,8 @@ from core.participant import *
 from core.placement import LaborPlacement
 
 # helper function used further below
-def produce(name : str, production_coefficients : Bundle, wage_per_worker : float, prices : Prices) -> Tuple[float, VolumeBundle]:
+def produce(name: str, production_coefficients: Bundle, wage_per_worker: float,
+            prices: Prices) -> Tuple[float, VolumeBundle]:
     income_rate = production_coefficients @ prices
     if (income_rate <= 0):
         #logging.debug(f"{name}: income_rate: {income_rate}")
@@ -26,27 +27,27 @@ class Producer(Participant):
                 placement : LaborPlacement):
         wide_pcs = np.zeros(placement.global_width)
         wide_pcs[placement.production_slice] = production_coefficients
-        return Producer(name, wide_pcs, placement.labour_index)
+        return Producer(name, wide_pcs, placement.labor_index)
 
     @classmethod
-    def trader(cls, name : str, labour_index : int, from_index : int,
+    def trader(cls, name : str, labor_index : int, from_index : int,
                to_index : int, trade_efficiency : float, global_width : int):
         wide_pcs = np.zeros(global_width)
         wide_pcs[from_index] = -trade_efficiency
         wide_pcs[to_index] = trade_efficiency
-        return Producer(name, wide_pcs, labour_index)
+        return Producer(name, wide_pcs, labor_index)
 
     def __init__(self, name : str, production_coefficients : Bundle,
-                 labour_index : int):
+                 labor_index : int):
         self.name = name 
         self.production_coefficients = production_coefficients
-        self.labour_index = labour_index
-        # could this also be implemented if we make labour part of the production_coefficients
+        self.labor_index = labor_index
+        # could this also be implemented if we make labor part of the production_coefficients
 
     def participate(self, prices : Prices) -> VolumeBundle:
         (workforce,production) = produce(self.name,
                                          self.production_coefficients,
-                                         prices[self.labour_index],
+                                         prices[self.labor_index],
                                          prices)
-        production.add_at_ix(self.labour_index, -workforce)
+        production.add_at_ix(self.labor_index, -workforce)
         return production